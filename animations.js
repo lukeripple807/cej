c0b15c2f05093a9c2f16493d3ae08826127c99c0
Ascensionbanks <lukeripple44@gmail.com>
2024-02-13 23:12:38 -0500

    sdsdsd

---
+++ animations.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var element = document.querySelector('.lineUp');
+    element.style.opacity = '1'; // Set opacity to 1 to reveal the element
+    element.style.transform = 'translateY(0%)'; // Reset transform to initial position
+  });
